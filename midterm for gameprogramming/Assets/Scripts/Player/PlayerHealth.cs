2847267a0502decabccd040b9f06c094241917ae
Alex <ahuberde@my.centennialcollege.ca>
2025-10-15 13:38:28 -0500

    added hp ui and ported enemy scripts

---
+++ midterm for gameprogramming/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 5;
+    void Start()
+    {
+        health = maxHealth;
+    }
+    public void takeDamage(int amount)
+    {
+        health -= amount;
+        if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
