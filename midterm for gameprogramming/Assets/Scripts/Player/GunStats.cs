ec4735025aab3992d7602ef578cf64298ccf17c6
Nick <nickwagner233@gmail.com>
2025-10-16 21:45:41 -0400

    Gun variants/level setup

--- midterm for gameprogramming/Assets/Scripts/Player/GunStats.cs
+++ midterm for gameprogramming/Assets/Scripts/Player/GunStats.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 
 public class GunStats : ScriptableObject
 {
-   
+
     [Header("Stats")]
     public float fireRate = 0.2f;
     public int bulletsPerShot = 1;
+    public int burstFireAmount = 1;
+    public float burstFireDelay = 0.1f;
     public float spreadAngle = 5f;
     public float bulletSpeed = 10f;
     public float bulletDamage = 1f;
     public float bulletLifeTime = 2f;
+    public int pierce = 0;
 
     public float KBForce = 0f;
     public Vector2 KBAngle = Vector2.zero;
--- midterm for gameprogramming/Assets/Scripts/Player/PlayerGun.cs
+++ midterm for gameprogramming/Assets/Scripts/Player/PlayerGun.cs
@@ -19,20 +19,24 @@ public class PlayerGun : MonoBehaviour
     {
         if (InputManager.AttackWasPressed && Time.time >= nextFireTime)
         {
-            Shoot();
+            StartCoroutine(Shoot());
             nextFireTime = Time.time + stats.fireRate;
         }
     }
 
-    void Shoot()
+    private IEnumerator Shoot()
     {
-        for (int i = 0; i < stats.bulletsPerShot; i++)
+        for (int i = 0; i < stats.burstFireAmount; i++)
         {
-            float angle = Random.Range(-stats.spreadAngle / 2, stats.spreadAngle / 2);
-            Quaternion rotation = firePoint.rotation * Quaternion.Euler(0, 0, angle);
-            GameObject bulletObj = Instantiate(stats.bulletPrefab, firePoint.position, rotation);
-            Bullet bullet = bulletObj.GetComponent<Bullet>();
-            bullet.Init(stats.bulletDamage, stats.bulletSpeed, stats.bulletLifeTime, stats.KBForce, stats.KBAngle, stats.DamageableLayer, playerMove.isFacingRight);
+            for (int n = 0; n < stats.bulletsPerShot; n++)
+            {
+                float angle = Random.Range(-stats.spreadAngle / 2, stats.spreadAngle / 2);
+                Quaternion rotation = firePoint.rotation * Quaternion.Euler(0, 0, angle);
+                GameObject bulletObj = Instantiate(stats.bulletPrefab, firePoint.position, rotation);
+                Bullet bullet = bulletObj.GetComponent<Bullet>();
+                bullet.Init(stats.bulletDamage, stats.bulletSpeed, stats.bulletLifeTime, stats.pierce, stats.KBForce, stats.KBAngle, stats.DamageableLayer, playerMove.isFacingRight);
+            }
+            yield return new WaitForSeconds(stats.burstFireDelay);
         }
     }
 
